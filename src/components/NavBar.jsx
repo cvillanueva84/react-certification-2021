347e8bf72c69eba8f04552bcd9e225595ead749b
Martin Eduardo Santos Perea <martin0604@hotmail.com>
2021-07-26 18:58:38 -0500

    First Deliverable

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const NavBar = ({ className }) => {
+  return (
+    <>
+      <div className={className}>
+        <div>
+          <div style={{marginRight:7, color:'white'}}>Home</div>
+          <input type="text" placeholder="Search" />
+        </div>
+
+        <div >
+          <div style={{marginRight:7, color:'white'}}>
+            <input id="modoVista" type="checkbox" name="modoVista" />
+            <label htmlFor="modoVista">DarkMode</label>
+          </div>
+          <div style={{color:'white'}}>Login</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const LightNavBar = styled(NavBar)`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    background-color: #1c5476;
+    padding: 10px;
+
+    @media screen and (min-width: 430px) {
+        flex-direction: row;
+    }
+
+    @media screen and (max-width: 430px) {
+        flex-direction: column;
+    }
+
+    > div {
+        display: flex;
+        justify-content: space-around;
+    }
+`;
+
+const DarkNavBar = styled(NavBar)`
+    display: flex;
+    justify-content: space-between;
+    background-color: #556cd6;
+    padding: 10;
+
+    > div {
+        display: flex;
+        justify-content: space-around;
+    }
+`;
+
+export default {DarkNavBar, LightNavBar};
--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,38 +1,23 @@
-import React, { useRef } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-
-import { useAuth } from '../../providers/Auth';
-import './Home.styles.css';
+import React from 'react';
+import StyledCardVideo from '../../components/CardVideo';
+import { youtubevideos } from '../../mocks/youtubevideos';
 
 function HomePage() {
-  const history = useHistory();
-  const sectionRef = useRef(null);
-  const { authenticated, logout } = useAuth();
-
-  function deAuthenticate(event) {
-    event.preventDefault();
-    logout();
-    history.push('/');
-  }
 
   return (
-    <section className="homepage" ref={sectionRef}>
-      <h1>Hello stranger!</h1>
-      {authenticated ? (
-        <>
-          <h2>Good to have you back</h2>
-          <span>
-            <Link to="/" onClick={deAuthenticate}>
-              ← logout
-            </Link>
-            <span className="separator" />
-            <Link to="/secret">show me something cool →</Link>
-          </span>
-        </>
-      ) : (
-        <Link to="/login">let me in →</Link>
-      )}
-    </section>
+    <>
+    <div style={{display: 'flex', justifyContent: 'center', flexWrap: 'wrap'}} >
+      {youtubevideos.map((video, index) => (
+        <StyledCardVideo
+        key={index} 
+        title={video.snippet.title} 
+        description={video.snippet.description} 
+        picture={video.snippet.thumbnails.medium.url}
+        />
+      ))
+      }
+    </div>
+    </>
   );
 }
 
