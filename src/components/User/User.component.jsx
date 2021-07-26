608ac5a32f1e6e16f61ccebdbde885519abc49b6
Fanks <hola@marm.dev>
2021-07-25 20:01:53 -0500

    First Deliverable: Core Concepts + Styling

---
+++ src/components/User/User.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const UserContainer = styled.div`
+  margin-left: 1em;
+  display: flex;
+  img {
+    border-radius: 50%;
+    height: 40px;
+    width: 40px;
+  }
+`;
+
+function User() {
+  return (
+    <UserContainer>
+      <img src="https://via.placeholder.com/100x100" alt="userPicture" />
+    </UserContainer>
+  );
+}
+
+export default User;
--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,23 +1,65 @@
 import React, { useRef } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+// import { Link, useHistory } from 'react-router-dom';
 
-import { useAuth } from '../../providers/Auth';
+// import { useAuth } from '../../providers/Auth';
+import styled from 'styled-components';
+import youtubeData from '../../youtube-videos-mock';
 import './Home.styles.css';
 
-function HomePage() {
-  const history = useHistory();
-  const sectionRef = useRef(null);
-  const { authenticated, logout } = useAuth();
+const VideoCard = styled.div`
+  background: #fff;
+  box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14),
+    0px 1px 3px 0px rgba(0, 0, 0, 0.12);
+  border-radius: 4px;
+  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  width: 345px;
+  margin: 15px;
 
-  function deAuthenticate(event) {
-    event.preventDefault();
-    logout();
-    history.push('/');
+  .videoTextContainer {
+    padding: 16px;
   }
+  .videoCardMedia {
+    height: 140px;
+    object-fit: cover;
+    width: 100%;
+  }
+  .videoCardTitle {
+    margin: 0;
+    font-size: 1.25rem;
+    font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+    font-weight: 500;
+    line-height: 1.6;
+    letter-spacing: 0.0075em;
+    margin-bottom: 0.35em;
+  }
+  .videoCardDescription {
+    font-size: 0.875rem;
+    font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+    font-weight: 400;
+    line-height: 1.43;
+    letter-spacing: 0.01071em;
+    color: rgba(0, 0, 0, 0.54);
+  }
+  &:hover {
+    opacity: 0.75;
+    cursor: pointer;
+  }
+`;
+
+function HomePage() {
+  // const history = useHistory();
+  const sectionRef = useRef(null);
+  // const { authenticated, logout } = useAuth();
+
+  // function deAuthenticate(event) {
+  //   event.preventDefault();
+  //   logout();
+  //   history.push('/');
+  // }
 
   return (
     <section className="homepage" ref={sectionRef}>
-      <h1>Hello stranger!</h1>
+      {/* <h1>Hello stranger!</h1>
       {authenticated ? (
         <>
           <h2>Good to have you back</h2>
@@ -31,7 +73,24 @@ function HomePage() {
         </>
       ) : (
         <Link to="/login">let me in →</Link>
-      )}
+      )} */}
+      {youtubeData.items.map((video) => (
+        <VideoCard key={video.etag}>
+          <img
+            className="videoCardMedia"
+            src={video.snippet.thumbnails.high.url}
+            alt={video.kind}
+          />
+          <div className="videoTextContainer">
+            <h3 className="videoCardTitle">{video.snippet.title}</h3>
+            <span className="videoCardDescription">
+              {video.snippet.description
+                ? video.snippet.description
+                : 'Este video no cuenta con descripción'}
+            </span>
+          </div>
+        </VideoCard>
+      ))}
     </section>
   );
 }
