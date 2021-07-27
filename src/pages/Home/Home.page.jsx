f3a4d9f18d4d0c00b021a944e23a8f2123fa7bc4
Danielalab <olabolacrayola@hotmail.com>
2021-07-27 16:50:43 -0500

    feat(Home): add VideosList into Home component

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,38 +1,25 @@
-import React, { useRef } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import React from 'react';
+import styled from 'styled-components';
+import VideosList from '../../components/VideosList/VideosList';
 
-import { useAuth } from '../../providers/Auth';
-import './Home.styles.css';
+const Wrapper = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding: 2rem;
+  margin-top: 70px;
+  background-color: #fbe8d3;
+  @media (max-width: 576px) {
+    justify-content: center;
+    padding: 1rem;
+  }
+`;
 
 function HomePage() {
-  const history = useHistory();
-  const sectionRef = useRef(null);
-  const { authenticated, logout } = useAuth();
-
-  function deAuthenticate(event) {
-    event.preventDefault();
-    logout();
-    history.push('/');
-  }
-
   return (
-    <section className="homepage" ref={sectionRef}>
-      <h1>Hello stranger!</h1>
-      {authenticated ? (
-        <>
-          <h2>Good to have you back</h2>
-          <span>
-            <Link to="/" onClick={deAuthenticate}>
-              ← logout
-            </Link>
-            <span className="separator" />
-            <Link to="/secret">show me something cool →</Link>
-          </span>
-        </>
-      ) : (
-        <Link to="/login">let me in →</Link>
-      )}
-    </section>
+    <Wrapper>
+      <VideosList />
+    </Wrapper>
   );
 }
 
