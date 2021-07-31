0118bbf744b609d825ed29881e327deb57a03a8f
Danielalab <olabolacrayola@hotmail.com>
2021-07-31 16:54:14 -0500

    refactor(VideosList): change classes for styled components

--- src/components/VideosList/VideosList.component.jsx
+++ src/components/VideosList/VideosList.component.jsx
@@ -7,18 +7,6 @@ const { snippet: channel } = items.shift();
 const VideoCard = styled.div`
   max-width: 320px;
   margin: 0.75rem;
-  p {
-    margin: 0;
-    color: #555555;
-  }
-  .channel-avatar {
-    width: 40px;
-    height: 40px;
-    border-radius: 50%;
-  }
-  .margin-x {
-    margin: 0 0.75rem;
-  }
 `;
 
 const Wrapper = styled.div`
@@ -31,6 +19,21 @@ const Link = styled.a`
   font-weight: 700;
 `;
 
+const ChannelAvatar = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+`;
+
+const Text = styled.p`
+  margin: 0;
+  color: #555555;
+`;
+
+const MarginX = styled.div`
+  margin: 0 0.75rem;
+`;
+
 const VideosList = () =>
   items.map(({ id, snippet }) => (
     <VideoCard key={id.videoId}>
@@ -38,20 +41,16 @@ const VideosList = () =>
         <img src={snippet.thumbnails.medium.url} alt={`${snippet.title} thumbnail`} />
       </figure>
       <Wrapper>
-        <img
-          src={channel.thumbnails.default.url}
-          alt={channel.channelTitle}
-          className="channel-avatar"
-        />
-        <div className="margin-x">
+        <ChannelAvatar src={channel.thumbnails.default.url} alt={channel.channelTitle} />
+        <MarginX className="margin-x">
           <Link href={`https://www.youtube.com/watch?v=${id.videoId}`}>
             {snippet.title}
           </Link>
           <div>
-            <p>{snippet.channelTitle}</p>
-            <p>{snippet.publishedAt}</p>
+            <Text>{snippet.channelTitle}</Text>
+            <Text>{snippet.publishedAt}</Text>
           </div>
-        </div>
+        </MarginX>
       </Wrapper>
     </VideoCard>
   ));
