3e35bbf19ec82ec5b86c79a4a30a69885e76d567
Christian Alonso <alonaranda@gmail.com>
2021-08-16 23:22:30 -0500

    HCards:Hover > Button display

--- src/components/FavItem.js
+++ src/components/FavItem.js
@@ -1,63 +1,58 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import '../style/FavoriteVideosView.css'
+import '../style/FavoriteVideosView.css';
 import { GlobalContext } from './Context';
 
 export const FavItem = ({ title, description, url, videoId }) => {
+  const { myStateReducer, dispatch } = useContext(GlobalContext);
 
-    const { myStateReducer, dispatch } = useContext(GlobalContext);
+  const handleClickPlay = () => {
+    dispatch({
+      type: 'actionAddVideoDataFav',
+      payload: {
+        ...myStateReducer,
+        videoDataFav: {
+          title: title,
+          description: description,
+          url: url,
+          videoId: videoId,
+        },
+      },
+    });
+  };
 
-    const handleClickPlay = () => {
-        dispatch({
-            type: 'actionAddVideoDataFav',
-            payload: {
-                ...myStateReducer,
-                videoDataFav: {
-                    title: title,
-                    description: description,
-                    url: url,
-                    videoId: videoId
-                }
-            }
-        });
-    }
+  const handleClickDelete = () => {
+    dispatch({
+      type: 'actionDelete',
+      payload: videoId,
+    });
+  };
 
-    const handleClickDelete = () => {
-        dispatch({
-            type: 'actionDelete',
-            payload: videoId
-        });
-    }
+  return (
+    <div className="card-item">
+      <img src={url} alt={title}></img>
+      <h2>{title}</h2>
+      <p>{description}</p>
 
+      <div className="botones-favoritos">
+        <button onClick={handleClickPlay}>
+          <Link to={`/videodetailsfavs`}>
+            <i className="far fa-play-circle"></i>
+          </Link>
+        </button>
 
-    return (
-        <div className="card-item">
-            <img src={url} alt={title}></img>
-            <h2>{title}</h2>
-            <p>{description}</p>
-            <div className="botones-favoritos">
-                <button
-                    className="button-left"
-                    onClick={handleClickPlay}
-                ><Link
-                    to={`/videodetailsfavs`}
-                ><i className="far fa-play-circle"></i></Link></button>
-
-                <button
-                    className="button-right"
-                    onClick={handleClickDelete}
-                ><i className="far fa-trash-alt"></i></button>
-
-
-            </div>
-        </div>
-    );
+        <button onClick={handleClickDelete}>
+          <i className="far fa-trash-alt"></i>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 FavItem.prototype = {
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    url: PropTypes.string.isRequired,
-    videoId: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  videoId: PropTypes.string.isRequired,
 };
