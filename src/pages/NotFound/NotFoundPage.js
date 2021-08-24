17464f4eeb93cc655a113910b019ce223582243a
unknown <antonio.salazar.emrsn@gmail.com>
2021-08-24 14:36:22 -0500

    added the test of the PrivateComponent

--- src/pages/NotFound/NotFoundPage.js
+++ src/pages/NotFound/NotFoundPage.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { NotFoundSection } from './NotFoundPage.styled'
+import { NotFoundGif, NotFoundLink, NotFoundSection } from './NotFoundPage.styled'
 
 import './NotFoundPage.styles.css';
 
 function NotFoundPage() {
   return (
     <NotFoundSection >
-      <Link to="/" className="home-link">
+      <NotFoundLink to="/" >
         home
-      </Link>
-      <img src="404.gif" alt="page not found" />
+      </NotFoundLink>
+      <NotFoundGif src="404.gif" alt="page not found" />
     </NotFoundSection>
   );
 }
