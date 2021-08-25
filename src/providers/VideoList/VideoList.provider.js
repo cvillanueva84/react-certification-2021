b77c3e732c3c851249a447050cd116148bc6bea5
unknown <antonio.salazar.emrsn@gmail.com>
2021-08-25 11:59:03 -0500

    as per my mentor recommendation I have refactored the custom Hook useFetch, I have also refactored the VideoList Provider so it does not have the api call inside of it

--- src/providers/VideoList/VideoList.provider.js
+++ src/providers/VideoList/VideoList.provider.js
@@ -2,10 +2,12 @@ import React, { createContext, useEffect, useState, useReducer } from 'react';
 import { useHistory } from "react-router-dom";
 import { reducer } from './VideoList.reducer';
 import Swal from 'sweetalert2'
+import { useFetch } from '../../utils/hooks/useFetch';
 export const VideoListContext = createContext();
 
 const VideoListProvider = (props) => {
   const [search, setSearch] = useState('wizeline');
+  const { videos } = useFetch(search);
   const initialState = {
     loading: true,
     posts: [],
@@ -14,27 +16,11 @@ const VideoListProvider = (props) => {
   };
   const [videosState, dispatch] = useReducer(reducer, initialState);
   const history = useHistory()
-  useEffect(() => {
-    let mounted  = true;
-    const getVideos = async () => {
-        try {
-          const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&&key=${process.env.REACT_APP_GOOGLE_APP_API_KEY}&type=video&maxResults=23&q=${search}`;
-          const response = await fetch(url);
-          if (mounted) {
-            const data = await response.json();
-            dispatch({ type: 'FETCH_SUCCESS', payload: data.items });
-          }
-          history.push('/')
-        } catch (error) {
-          dispatch({ type: 'FETCH_ERROR' });
-          throw new Error(error);
-        }
-      
-    };
-    getVideos();
-    return () => mounted = false;
 
-  }, [search, history]);
+  useEffect(() => {
+    dispatch({ type: 'FETCH_SUCCESS', payload: videos });
+  }, [search, history, videos])
+
 
   const addToFavorites = (video) => {
     dispatch({
