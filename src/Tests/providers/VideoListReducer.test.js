90ed4348f911fed0d9e4b71aad538efb180f6129
unknown <antonio.salazar.emrsn@gmail.com>
2021-08-25 20:20:16 -0500

    fixing some parts of the code that broke after refactoring the useFetch hook

--- src/Tests/providers/VideoListReducer.test.js
+++ src/Tests/providers/VideoListReducer.test.js
@@ -47,14 +47,17 @@ describe('tests of the VideoList reducer actions', () => {
         const action = {
             type: 'ADD_TO_FAVORITES',
             payload: {
-                "kind": "youtube#video",
-                "etag": "TJsn_MhmnvgnXsO6vYt7tumHLqM",
-                "id": "nmXMgqjQzls",
+                "kind": "youtube#searchResult",
+                "etag": "erqeM78PZDWIBe8qOGHGM2WdSE8",
+                "id": {
+                    "kind": "youtube#video",
+                    "videoId": "nmXMgqjQzls"
+                },
                 "snippet": {
                     "publishedAt": "2019-09-30T23:54:32Z",
                     "channelId": "UCPGzT4wecuWM0BH9mPiulXg",
                     "title": "Video Tour | Welcome to Wizeline Guadalajara",
-                    "description": "Follow Hector Padilla, Wizeline Director of Engineering, for a lively tour of our office.\n\nIn 2018, Wizeline opened its stunning new office in Guadalajara, Jalisco, Mexico, home to over 400 employees. Wizeline has drawn attention for designing an office and culture focused on employee wellness and cultivating talent, earning a Super Espacios award by WeWork, Expansion Mexico, and Top Companies.\n\nRead more about the office here: https://www.wizeline.com/creating-first-class-facilities/\n\nIf you're interested in working with Wizeline and want to experience the culture and tour our offices live, contact us at www.wizeline.com/contact/",
+                    "description": "Follow Hector Padilla, Wizeline Director of Engineering, for a lively tour of our office. In 2018, Wizeline opened its stunning new office in Guadalajara, Jalisco, ...",
                     "thumbnails": {
                         "default": {
                             "url": "https://i.ytimg.com/vi/nmXMgqjQzls/default.jpg",
@@ -70,32 +73,11 @@ describe('tests of the VideoList reducer actions', () => {
                             "url": "https://i.ytimg.com/vi/nmXMgqjQzls/hqdefault.jpg",
                             "width": 480,
                             "height": 360
-                        },
-                        "standard": {
-                            "url": "https://i.ytimg.com/vi/nmXMgqjQzls/sddefault.jpg",
-                            "width": 640,
-                            "height": 480
-                        },
-                        "maxres": {
-                            "url": "https://i.ytimg.com/vi/nmXMgqjQzls/maxresdefault.jpg",
-                            "width": 1280,
-                            "height": 720
                         }
                     },
                     "channelTitle": "Wizeline",
-                    "tags": [
-                        "Mexican silicon valley",
-                        "silicon valley",
-                        "startup culture",
-                        "technology",
-                        "engineering culture"
-                    ],
-                    "categoryId": "28",
                     "liveBroadcastContent": "none",
-                    "localized": {
-                        "title": "Video Tour | Welcome to Wizeline Guadalajara",
-                        "description": "Follow Hector Padilla, Wizeline Director of Engineering, for a lively tour of our office.\n\nIn 2018, Wizeline opened its stunning new office in Guadalajara, Jalisco, Mexico, home to over 400 employees. Wizeline has drawn attention for designing an office and culture focused on employee wellness and cultivating talent, earning a Super Espacios award by WeWork, Expansion Mexico, and Top Companies.\n\nRead more about the office here: https://www.wizeline.com/creating-first-class-facilities/\n\nIf you're interested in working with Wizeline and want to experience the culture and tour our offices live, contact us at www.wizeline.com/contact/"
-                    }
+                    "publishTime": "2019-09-30T23:54:32Z"
                 }
             }
         }
@@ -104,14 +86,17 @@ describe('tests of the VideoList reducer actions', () => {
         expect(state).toEqual({
             loading: false,
             favoriteVideos: [{
-                "kind": "youtube#video",
-                "etag": "TJsn_MhmnvgnXsO6vYt7tumHLqM",
-                "id": "nmXMgqjQzls",
+                "kind": "youtube#searchResult",
+                "etag": "erqeM78PZDWIBe8qOGHGM2WdSE8",
+                "id": {
+                    "kind": "youtube#video",
+                    "videoId": "nmXMgqjQzls"
+                },
                 "snippet": {
                     "publishedAt": "2019-09-30T23:54:32Z",
                     "channelId": "UCPGzT4wecuWM0BH9mPiulXg",
                     "title": "Video Tour | Welcome to Wizeline Guadalajara",
-                    "description": "Follow Hector Padilla, Wizeline Director of Engineering, for a lively tour of our office.\n\nIn 2018, Wizeline opened its stunning new office in Guadalajara, Jalisco, Mexico, home to over 400 employees. Wizeline has drawn attention for designing an office and culture focused on employee wellness and cultivating talent, earning a Super Espacios award by WeWork, Expansion Mexico, and Top Companies.\n\nRead more about the office here: https://www.wizeline.com/creating-first-class-facilities/\n\nIf you're interested in working with Wizeline and want to experience the culture and tour our offices live, contact us at www.wizeline.com/contact/",
+                    "description": "Follow Hector Padilla, Wizeline Director of Engineering, for a lively tour of our office. In 2018, Wizeline opened its stunning new office in Guadalajara, Jalisco, ...",
                     "thumbnails": {
                         "default": {
                             "url": "https://i.ytimg.com/vi/nmXMgqjQzls/default.jpg",
@@ -127,32 +112,11 @@ describe('tests of the VideoList reducer actions', () => {
                             "url": "https://i.ytimg.com/vi/nmXMgqjQzls/hqdefault.jpg",
                             "width": 480,
                             "height": 360
-                        },
-                        "standard": {
-                            "url": "https://i.ytimg.com/vi/nmXMgqjQzls/sddefault.jpg",
-                            "width": 640,
-                            "height": 480
-                        },
-                        "maxres": {
-                            "url": "https://i.ytimg.com/vi/nmXMgqjQzls/maxresdefault.jpg",
-                            "width": 1280,
-                            "height": 720
                         }
                     },
                     "channelTitle": "Wizeline",
-                    "tags": [
-                        "Mexican silicon valley",
-                        "silicon valley",
-                        "startup culture",
-                        "technology",
-                        "engineering culture"
-                    ],
-                    "categoryId": "28",
                     "liveBroadcastContent": "none",
-                    "localized": {
-                        "title": "Video Tour | Welcome to Wizeline Guadalajara",
-                        "description": "Follow Hector Padilla, Wizeline Director of Engineering, for a lively tour of our office.\n\nIn 2018, Wizeline opened its stunning new office in Guadalajara, Jalisco, Mexico, home to over 400 employees. Wizeline has drawn attention for designing an office and culture focused on employee wellness and cultivating talent, earning a Super Espacios award by WeWork, Expansion Mexico, and Top Companies.\n\nRead more about the office here: https://www.wizeline.com/creating-first-class-facilities/\n\nIf you're interested in working with Wizeline and want to experience the culture and tour our offices live, contact us at www.wizeline.com/contact/"
-                    }
+                    "publishTime": "2019-09-30T23:54:32Z"
                 }
             }],
             error: '',
@@ -168,7 +132,40 @@ describe('tests of the VideoList reducer actions', () => {
         const initialState = {
             loading:false,
             posts: [],
-            favoriteVideos: [{"kind":"youtube#video","etag":"TJsn_MhmnvgnXsO6vYt7tumHLqM","id":"nmXMgqjQzls","snippet":{"publishedAt":"2019-09-30T23:54:32Z","channelId":"UCPGzT4wecuWM0BH9mPiulXg","title":"Video Tour | Welcome to Wizeline Guadalajara","description":"Follow Hector Padilla, Wizeline Director of Engineering, for a lively tour of our office.\n\nIn 2018, Wizeline opened its stunning new office in Guadalajara, Jalisco, Mexico, home to over 400 employees. Wizeline has drawn attention for designing an office and culture focused on employee wellness and cultivating talent, earning a Super Espacios award by WeWork, Expansion Mexico, and Top Companies.\n\nRead more about the office here: https://www.wizeline.com/creating-first-class-facilities/\n\nIf you're interested in working with Wizeline and want to experience the culture and tour our offices live, contact us at www.wizeline.com/contact/","thumbnails":{"default":{"url":"https://i.ytimg.com/vi/nmXMgqjQzls/default.jpg","width":120,"height":90},"medium":{"url":"https://i.ytimg.com/vi/nmXMgqjQzls/mqdefault.jpg","width":320,"height":180},"high":{"url":"https://i.ytimg.com/vi/nmXMgqjQzls/hqdefault.jpg","width":480,"height":360},"standard":{"url":"https://i.ytimg.com/vi/nmXMgqjQzls/sddefault.jpg","width":640,"height":480},"maxres":{"url":"https://i.ytimg.com/vi/nmXMgqjQzls/maxresdefault.jpg","width":1280,"height":720}},"channelTitle":"Wizeline","tags":["Mexican silicon valley","silicon valley","startup culture","technology","engineering culture"],"categoryId":"28","liveBroadcastContent":"none","localized":{"title":"Video Tour | Welcome to Wizeline Guadalajara","description":"Follow Hector Padilla, Wizeline Director of Engineering, for a lively tour of our office.\n\nIn 2018, Wizeline opened its stunning new office in Guadalajara, Jalisco, Mexico, home to over 400 employees. Wizeline has drawn attention for designing an office and culture focused on employee wellness and cultivating talent, earning a Super Espacios award by WeWork, Expansion Mexico, and Top Companies.\n\nRead more about the office here: https://www.wizeline.com/creating-first-class-facilities/\n\nIf you're interested in working with Wizeline and want to experience the culture and tour our offices live, contact us at www.wizeline.com/contact/"}}}],
+            favoriteVideos: [{
+                "kind": "youtube#searchResult",
+                "etag": "erqeM78PZDWIBe8qOGHGM2WdSE8",
+                "id": {
+                    "kind": "youtube#video",
+                    "videoId": "nmXMgqjQzls"
+                },
+                "snippet": {
+                    "publishedAt": "2019-09-30T23:54:32Z",
+                    "channelId": "UCPGzT4wecuWM0BH9mPiulXg",
+                    "title": "Video Tour | Welcome to Wizeline Guadalajara",
+                    "description": "Follow Hector Padilla, Wizeline Director of Engineering, for a lively tour of our office. In 2018, Wizeline opened its stunning new office in Guadalajara, Jalisco, ...",
+                    "thumbnails": {
+                        "default": {
+                            "url": "https://i.ytimg.com/vi/nmXMgqjQzls/default.jpg",
+                            "width": 120,
+                            "height": 90
+                        },
+                        "medium": {
+                            "url": "https://i.ytimg.com/vi/nmXMgqjQzls/mqdefault.jpg",
+                            "width": 320,
+                            "height": 180
+                        },
+                        "high": {
+                            "url": "https://i.ytimg.com/vi/nmXMgqjQzls/hqdefault.jpg",
+                            "width": 480,
+                            "height": 360
+                        }
+                    },
+                    "channelTitle": "Wizeline",
+                    "liveBroadcastContent": "none",
+                    "publishTime": "2019-09-30T23:54:32Z"
+                }
+            }],
             error: ''
         }
 
--- src/utils/hooks/useFetch.js
+++ src/utils/hooks/useFetch.js
@@ -1,27 +1,25 @@
 import { useEffect, useState } from 'react'
 import { useHistory } from "react-router-dom";
-
+import axios from 'axios'
 export const useFetch = (search) => {
     const [videos, setVideos] = useState([])
     const history = useHistory()
-
 
     useEffect(() => {
         const getYoutubeVideos = async () => {
             try {
                 const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&&key=${process.env.REACT_APP_GOOGLE_APP_API_KEY}&type=video&maxResults=23&q=${search}`
-                const resp = await fetch(url)
-                const data = await resp.json()
-                setVideos(data.items);
+                const resp = await axios.get(url)
+                const data = resp.data.items
+                setVideos(data);
                 history.push('/')
             } catch (error) {
                 throw new Error(error)
             }
-
         }
-        getYoutubeVideos()
-    }, [search, history])
+        getYoutubeVideos()    
 
+    }, [search, history])
     return {
         videos
     }
