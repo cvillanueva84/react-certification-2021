d6c8bf7fee6ecfe7f77c1fc45b56ee65b958c239
Luis Erick Ivan Palafox Vega <erickpalafox@Luiss-MacBook-Pro.local>
2021-11-05 14:09:21 -0600

    Mini-Challenge 1: Lint corrections

--- src/components/Layout/Content.component.jsx
+++ src/components/Layout/Content.component.jsx
@@ -2,24 +2,23 @@ import React from 'react';
 
 import { BlockElement, ContentWrapper, Description, VideoThumbnail } from './Content.styles';
 
-let data = require('../../mockdata/youtube-videos-mock.json');
+const data = require('../../mockdata/youtube-videos-mock.json');
 
 function ContentComponent() {
-    console.log(data.items.lenght)
     const element = data && data.items.map((item)=>(
-        <BlockElement>
-            <VideoThumbnail src={item.snippet.thumbnails.medium.url} title={item.snippet.title} />
-            <Description>
-                <h3>{item.snippet.title}</h3>
-                <div>{item.snippet.description}</div>
-            </Description>
-        </BlockElement>
+      <BlockElement>
+        <VideoThumbnail src={item.snippet.thumbnails.medium.url} title={item.snippet.title} />
+        <Description>
+          <h3>{item.snippet.title}</h3>
+          <div>{item.snippet.description}</div>
+        </Description>
+      </BlockElement>
         
     ))
     return (
-        <ContentWrapper>
-            {element}
-        </ContentWrapper>
+      <ContentWrapper>
+        {element}
+      </ContentWrapper>
     )
 }
 
--- src/components/Layout/Header.component.jsx
+++ src/components/Layout/Header.component.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 
+import { faBars, faToggleOff } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ButtonToggle, HeaderWrapper, Input, LogoLink, MenuToggle } from './Header.styles';
 // import InsideSessionLogo from '../../resources/logo.png'; // TODO: Uncomment when login is ready
 import SessionOutLogo from '../../resources/session_out.png';
-import { faBars, faToggleOff } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function HeaderComponent() {
   return (
     <HeaderWrapper>
-      <Input type='text' placeholder={'Wizleine'}/>
+      <Input type='text' placeholder="Wizleine" />
       <ButtonToggle>
-        <FontAwesomeIcon icon={faToggleOff} size="6x" style={{ color: 'white'}}/>
+        <FontAwesomeIcon icon={faToggleOff} size="6x" style={{ color: 'white'}} />
       </ButtonToggle>
       Dark Mode
       <LogoLink href="#">
         <img src={SessionOutLogo} alt="Logo" />
       </LogoLink>
       <MenuToggle href="#">
-        <FontAwesomeIcon icon={faBars} size="6x" style={{ color: 'white'}}/>
+        <FontAwesomeIcon icon={faBars} size="6x" style={{ color: 'white'}} />
       </MenuToggle>
     </HeaderWrapper>
   );
